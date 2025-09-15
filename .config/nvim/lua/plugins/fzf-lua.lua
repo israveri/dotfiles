c7b75ebd6f569ffc1be20a80acba1f01284f78ec
Israel Ribeiro <israveri@gmail.com>
2025-09-15 18:12:20 -0300

    Updates nvim config
    Adds and fizes fzf-lua searches and keymaps

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -1,10 +1,25 @@
 return {
     "ibhagwan/fzf-lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    cmd = "FzfLua",
+    event = "VeryLazy",
     keys = {
-	{ "<C-p>", function() require("fzf-lua").files() end, desc = "Find file in current working directory" },
-	{ "<leader>fg", function() require("fzf-lua").live_grep() end, desc = "Find by grepping in current working directory" },
-	{ "<leader><C-,>", function() require("fzf-lua").files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find config files from anywhere" },
+	-- Keymaps for fuzzy search of files
+	{ "<C-p>",      function() require("fzf-lua").files() end, desc = "Search file" },
+	{ "<leader>ff", function() require("fzf-lua").files() end, desc = "Search file" },
+
+	-- Keymap to be able to serch neovim confi files from anywhere in the system
+	{ "<leader>fc", function() require("fzf-lua").files({ cmd = vim.fn.stdpath("config") }) end, desc = "Searh in neovim config" },
+
+	-- Other useful keymaps
+	{ "<leader>fg", function() require("fzf-lua").grep() end, desc = "Search with grep" },
+	{ "<leader>fl", function() require("fzf-lua").live_grep() end, desc = "Search with live grep" },
+	{ "<leader>fs", function() require("fzf-lua").lgrep_curbuf() end, desc = "Searh in current buffer" },
+	{ "<leader>fo", function() require("fzf-lua").buffers() end, desc = "Searh in opened buffers" },
+
     },
-    opts = {},
+    opts = {
+	files = { hidden = true },
+	grep = { hidden = true }
+    },
 }
