da54edebb41e8fad7defdcedf253ca9a41897e39
Israel Ribeiro <israveri@gmail.com>
2025-09-23 22:46:00 -0300

    Organizes blink and conform plugins config

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -1,38 +1,28 @@
 return {
-	"stevearc/conform.nvim",
-	keys = {
-		{
-			"<leader>cf",
-			function()
-				require("conform").format()
-			end,
-			desc = "Conform: [F]ormat file",
-		},
+    "stevearc/conform.nvim",
+    keys = {
+	{
+	    "<leader>cf",
+	    function()
+		require("conform").format()
+	    end,
+	    desc = "Conform: [F]ormat file",
 	},
-	opts = {
-		format_on_save = {
-			timeout_ms = 1000,
-			lsp_format = "fallback",
-		},
-		formatters_by_ft = {
-			bash = { "shfmt" },
-			lua = { "emmylua_ls" },
-			ruby = { "standardrb" },
-			sh = { "shfmt" },
-			html = { "prettierd" },
-			css = { "prettierd" },
-			javascript = { "prettierd" },
-			typescript = { "prettierd" },
-		},
-		
-		formatters = {
-			-- emmylua_ls = {
-			-- 	command = function()
-			-- 		return function(bufnr)
-			-- 			vim.lsp.buf.format({ bufnr = bufnr })
-			-- 		end
-			-- 	end
-			-- },
-		},
+    },
+    opts = {
+	format_on_save = {
+	    timeout_ms = 1000,
+	    lsp_format = "fallback",
 	},
+	formatters_by_ft = {
+	    bash = { "shfmt" },
+	    lua = { "emmylua_ls" },
+	    ruby = { "standardrb" },
+	    sh = { "shfmt" },
+	    html = { "prettierd" },
+	    css = { "prettierd" },
+	    javascript = { "prettierd" },
+	    typescript = { "prettierd" },
+	},
+    }
 }
