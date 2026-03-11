48a48cb725f82cc8b48bfa88f86534942bb669bc
Israel Ribeiro <israveri@gmail.com>
2026-03-11 20:01:44 -0300

    Huge update on how dotfiles are stow and installed

---
+++ stow/zsh/dot-config/zsh/shell/alias/core.zsh
@@ -0,0 +1,4 @@
+alias reload="exec zsh"
+alias dot="stow --dotfiles --dir=$HOME/.dotfiles/stow --target=$HOME"
+alias undot="stow --dotfiles --dir=$HOME/.dotfiles/stow --target=$HOME --delete"
+alias redot="stow --dotfiles --dir=$HOME/.dotfiles/stow --target=$HOME --restow"
