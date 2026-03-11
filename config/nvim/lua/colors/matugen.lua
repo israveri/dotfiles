1ef787cd2091b5565cf352bf18e234568b01813b
Israel Ribeiro <israveri@gmail.com>
2026-03-11 14:31:03 -0300

    Some updates to current setup with noctalia

--- config/nvim/lua/colors/matugen.lua
+++ config/nvim/lua/colors/matugen.lua
@@ -3,24 +3,24 @@ local M = {}
 function M.setup()
   require('base16-colorscheme').setup {
     -- Background tones
-    base00 = '#1e1e2e', -- Default Background
-    base01 = '#313244', -- Lighter Background (status bars)
-    base02 = '#3a3b50', -- Selection Background
-    base03 = '#646789', -- Comments, Invisibles
+    base00 = '#fffaf3', -- Default Background
+    base01 = '#f2e9e1', -- Lighter Background (status bars)
+    base02 = '#ecdfd3', -- Selection Background
+    base03 = '#9e8e8b', -- Comments, Invisibles
     -- Foreground tones
-    base04 = '#a3b4eb', -- Dark Foreground (status bars)
-    base05 = '#cdd6f4', -- Default Foreground
-    base06 = '#cdd6f4', -- Light Foreground
-    base07 = '#cdd6f4', -- Lightest Foreground
+    base04 = '#797593', -- Dark Foreground (status bars)
+    base05 = '#575279', -- Default Foreground
+    base06 = '#575279', -- Light Foreground
+    base07 = '#575279', -- Lightest Foreground
     -- Accent colors
-    base08 = '#f38ba8', -- Variables, XML Tags, Errors
-    base09 = '#94e2d5', -- Integers, Constants
-    base0A = '#fab387', -- Classes, Search Background
-    base0B = '#cba6f7', -- Strings, Diff Inserted
-    base0C = '#96e9db', -- Regex, Escape Chars
-    base0D = '#bb8af4', -- Functions, Methods
-    base0E = '#fab185', -- Keywords, Storage
-    base0F = '#c8043a', -- Deprecated, Embedded Tags
+    base08 = '#b4637a', -- Variables, XML Tags, Errors
+    base09 = '#286983', -- Integers, Constants
+    base0A = '#56949f', -- Classes, Search Background
+    base0B = '#d7827e', -- Strings, Diff Inserted
+    base0C = '#1b627e', -- Regex, Escape Chars
+    base0D = '#7e1f1b', -- Functions, Methods
+    base0E = '#1b6f7e', -- Keywords, Storage
+    base0F = '#e4cdd4', -- Deprecated, Embedded Tags
   }
 end
 
