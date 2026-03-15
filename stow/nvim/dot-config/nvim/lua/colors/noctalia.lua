6eaa0d7e4c1d5d0ecc85636356de1530eb0d30a1
Israel Ribeiro <israveri@gmail.com>
2026-03-15 03:48:35 -0300

    Update some colors

--- stow/nvim/dot-config/nvim/lua/colors/noctalia.lua
+++ stow/nvim/dot-config/nvim/lua/colors/noctalia.lua
@@ -3,26 +3,26 @@ local M = {}
 function M.setup()
   require('base16-colorscheme').setup {
     -- Background tones
-    base00 = '#0b0e14',	-- Default Background
-    base01 = '#1e222a',	-- Lighter Background (status bars)
-    base02 = '#262c36',	-- Selection Background
-    base03 = '#595f6a',	-- Comments, Invisibles
+    base00 = '#282828',	-- Default Background
+    base01 = '#3c3836',	-- Lighter Background (status bars)
+    base02 = '#474240',	-- Selection Background
+    base03 = '#786f6b',	-- Comments, Invisibles
 
     -- Foreground tones
-    base04 = '#8e959e',	-- Dark Foreground (status bars)
-    base05 = '#d1d1c7',	-- Default Foreground
-    base06 = '#d1d1c7',	-- Light Foreground
-    base07 = '#d1d1c7',	-- Lightest Foreground
+    base04 = '#ebdbb2',	-- Dark Foreground (status bars)
+    base05 = '#fbf1c7',	-- Default Foreground
+    base06 = '#fbf1c7',	-- Light Foreground
+    base07 = '#fbf1c7',	-- Lightest Foreground
 
     -- Accent colors
-    base08 = '#d95757',	-- Variables, XML Tags, Errors
-    base09 = '#39bae6',	-- Integers, Constants
-    base0A = '#aad94c',	-- Classes, Search Background
-    base0B = '#e6b450',	-- Strings, Diff Inserted
-    base0C = '#8ed8f1',	-- Regex, Escape Chars
-    base0D = '#efcf8f',	-- Functions, Methods
-    base0E = '#cde996',	-- Keywords, Storage
-    base0F = '#700e0e',	-- Deprecated, Embedded Tags
+    base08 = '#fb4934',	-- Variables, XML Tags, Errors
+    base09 = '#83a598',	-- Integers, Constants
+    base0A = '#fabd2f',	-- Classes, Search Background
+    base0B = '#b8bb26',	-- Strings, Diff Inserted
+    base0C = '#96e9c9',	-- Regex, Escape Chars
+    base0D = '#e8e995',	-- Functions, Methods
+    base0E = '#fcd782',	-- Keywords, Storage
+    base0F = '#7d0d00',	-- Deprecated, Embedded Tags
   }
 end
 
