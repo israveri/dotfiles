e7c9a0487cf45ebff80dea85c86685283f545335
Israel Ribeiro <israveri@gmail.com>
2026-03-18 01:45:11 -0300

    Adds system alias to zsh

--- stow/zsh/dot-config/zsh/shell/alias/core.zsh
+++ stow/zsh/dot-config/zsh/shell/alias/core.zsh
@@ -1,1 +1,11 @@
 alias reload="exec zsh"
+
+# Package Management
+alias cleanup="paru -Rsn $(paru -Qtdq)"
+alias rmpkg="paru -Rsn"
+alias update="paru -Syu"
+
+# System Control
+alias jctl="journalctl"
+alias jctlerr="journalctl -p 3 -xb"
+alias sctl="systemctl"
