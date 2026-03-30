48e378bfe2b2943c41b4c87ff3ec54cfdeb2cd0c
Israel Ribeiro <israveri@gmail.com>
2026-03-29 23:59:02 -0300

    Update tdl function to not create ai pane for now

--- stow/zsh/dot-config/zsh/shell/alias/applications/tmux.zsh
+++ stow/zsh/dot-config/zsh/shell/alias/applications/tmux.zsh
@@ -1,6 +1,15 @@
 alias t="tmux attach || tmux new -s Work"
 
 # Tmux Dev Layout
+# |--------------|-----|
+# |              |     |
+# |  editor      | ai  |
+# |              |     |
+# |              |     |
+# |--------------|     |
+# |              |     |
+# |  terminal    |     |
+# |--------------|-----|
 tdl() {
     [[ -z "$TMUX" ]] && { echo "Tmux must be running to use tdl"; return 1 }
 
@@ -12,16 +21,16 @@ tdl() {
     editor_pane="${TMUX_PANE}"
 
     # Creates ai split pane with 25% screen and grab its id
-    ai_pane=$(tmux split-window -h -p 25 -t "$editor_pane" -c "$current_dir" -P -F "#{pane_id}")
+    #ai_pane=$(tmux split-window -h -p 25 -t "$editor_pane" -c "$current_dir" -P -F "#{pane_id}")
 
-    # Creates terminal pane with 20% screen
-    tmux split-window -v -p 20 -t "$editor_pane" -c "$current_dir"
+    # Creates terminal pane with 25% screen
+    tmux split-window -v -p 25 -t "$editor_pane" -c "$current_dir"
 
     # Opens editor
     tmux send-keys -t "$editor_pane" "$EDITOR ." C-m
 
     # Opens ai
-    tmux send-keys -t "$ai_pane" "$ai ." C-m
+    #tmux send-keys -t "$ai_pane" "$ai ." C-m
 
     # Focus the editor pane
     tmux select-pane -t "$editor_pane"
