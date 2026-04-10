cf1ab5f4c798e2bb3efbef74af38f4b16a402cde
Israel Ribeiro <israveri@gmail.com>
2026-04-10 04:42:08 -0300

    Update zsh alias and bindings

--- stow/zsh/dot-config/zsh/shell/alias/unix.zsh
+++ stow/zsh/dot-config/zsh/shell/alias/unix.zsh
@@ -2,10 +2,13 @@ alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 
-alias ls="eza --header --long --git --classify --group-directories-first --color=always"
+alias ls="eza --icons --header --long --git --classify --group-directories-first --color=always"
 alias lsa="ls --all"
-alias lt="ls --tree -L1 --icons --no-permissions --no-filesize --no-user --no-time --no-git"
+alias lt="ls --tree -L1 --no-permissions --no-filesize --no-user --no-time --no-git"
 alias lta="lt --all"
 
 alias cat="bat"
-alias grep="grep --color=always"
+
+alias grep="rg -uu --smart-case --color=always"
+alias rgrep="rg --smart-case --color=always"
+alias pgrep="rg --hidden --smart-case --color=always"
--- stow/zsh/dot-config/zsh/shell/bindings.zsh
+++ stow/zsh/dot-config/zsh/shell/bindings.zsh
@@ -4,10 +4,12 @@ bindkey -e
 ############
 # Movement
 ############
-bindkey '^[[1;5D' backward-word  # Left arrow
-bindkey '^[[1;5C' forward-word   # Right arrow
-bindkey '^[[H' beginning-of-line # Home key
-bindkey '^[[F' end-of-line       # End key
-bindkey '^[[3~' delete-char      # Delete key
+bindkey '^[[1;5D' backward-word                # Left arrow
+bindkey '^[[1;5C' forward-word                 # Right arrow
+bindkey '^[[3~' delete-char                    # Delete key
+bindkey '^[[1~' beginning-of-line              # Home key
+bindkey '^[[4~' end-of-line                    # End key
+bindkey '^[[5~' beginning-of-line-hist-search  # Pg Up
+bindkey '^[[6~' end-of-line-hist-search        # Pg Dn
 
 bindkey '^p' autosuggest-accept # Ctrl+P accepts suggestion
