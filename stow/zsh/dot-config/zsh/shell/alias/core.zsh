c87920fef7688e25aa54a54bd9084b25a47aa52e
Israel Ribeiro <israveri@gmail.com>
2026-04-10 23:42:17 -0300

    Adds manifest alias to install packages

--- stow/zsh/dot-config/zsh/shell/alias/core.zsh
+++ stow/zsh/dot-config/zsh/shell/alias/core.zsh
@@ -2,6 +2,7 @@ alias reload="exec zsh"
 
 # Package Management
 alias cleanup="paru -Rsn $(paru -Qtdq)"
+alias manifest="paru -S --noconfirm --needed - < \"$HOME/.dotfiles/src/packages/manifest.txt\""
 alias rmpkg="paru -Rsn"
 alias update="paru -Syu"
 
