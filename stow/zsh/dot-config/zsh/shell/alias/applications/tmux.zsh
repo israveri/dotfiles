dfb00f6f97817671311e2ce88ecf945ac167d71b
Israel Ribeiro <israveri@gmail.com>
2026-04-14 23:16:14 -0300

    Update terminal configs

--- stow/zsh/dot-config/zsh/shell/alias/applications/tmux.zsh
+++ stow/zsh/dot-config/zsh/shell/alias/applications/tmux.zsh
@@ -1,4 +1,4 @@
-alias t="tmux attach || tmux new -s Work"
+alias t="tmux attach || tmux new -s $(basename $PWD)"
 
 # Tmux Dev Layout
 # |--------------|-----|
@@ -6,32 +6,34 @@ alias t="tmux attach || tmux new -s Work"
 # |  editor      | ai  |
 # |              |     |
 # |              |     |
-# |--------------|     |
 # |              |     |
-# |  terminal    |     |
 # |--------------|-----|
+# |  terminal          |
+# |--------------------|
 tdl() {
     [[ -z "$TMUX" ]] && { echo "Tmux must be running to use tdl"; return 1 }
 
     local editor_pane ai_pane
     local current_dir="${PWD}"
-    local ai="opencode"
+    local ai="pi"
 
     # Grabs the pane id
     editor_pane="${TMUX_PANE}"
 
-    # Creates ai split pane with 25% screen and grab its id
-    #ai_pane=$(tmux split-window -h -p 25 -t "$editor_pane" -c "$current_dir" -P -F "#{pane_id}")
-
     # Creates terminal pane with 25% screen
     tmux split-window -v -p 25 -t "$editor_pane" -c "$current_dir"
+
+    # Creates ai split pane with 25% screen and grab its id
+    ai_pane=$(tmux split-window -h -p 25 -t "$editor_pane" -c "$current_dir" -P -F "#{pane_id}")
 
     # Opens editor
     tmux send-keys -t "$editor_pane" "$EDITOR ." C-m
 
     # Opens ai
-    #tmux send-keys -t "$ai_pane" "$ai ." C-m
+    tmux send-keys -t "$ai_pane" "$ai ." C-m
 
     # Focus the editor pane
     tmux select-pane -t "$editor_pane"
+
+    tmux rename-session "$(basename $current_dir)"
 }
