edd4a89eaf0df1af01182c3ae7475e41de9813ee
Israel Ribeiro <israveri@gmail.com>
2026-06-13 17:23:14 -0300

    Remove unused functions from hyprland

--- stow/hypr/dot-config/hypr/functions/move_window_to_workspace_scratchpad.lua
+++
@@ -1,9 +0,0 @@
-local function f()
-    local workspace = hl.get_active_workspace()
-
-    if workspace ~= nil then
-        hl.dispatch(hl.dsp.window.move({ workspace = "special:" .. tostring(workspace.id) }))
-    end
-end
-
-return f
--- stow/hypr/dot-config/hypr/functions/toggle_workspace_scratchpad.lua
+++
@@ -1,9 +0,0 @@
-local function f()
-    local workspace = hl.get_active_workspace()
-
-    if workspace ~= nil then
-        hl.dispatch(hl.dsp.workspace.toggle_special(workspace.id))
-    end
-end
-
-return f
