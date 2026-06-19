21ae083cfe33700f6b93c748c1b83a79b949560c
Israel Ribeiro <israveri@gmail.com>
2026-06-19 20:44:13 -0300

    Fixes discord notification stealing focus

--- stow/hypr/dot-config/hypr/core/rules/discord.lua
+++ stow/hypr/dot-config/hypr/core/rules/discord.lua
@@ -8,3 +8,12 @@ hl.window_rule({
     size = { 450, 65 },
     move = { "monitor_w * 0.5 - 225", "monitor_h * 1 - 77" },
 })
+
+hl.on("workspace.active", function(ws)
+    for _, w in ipairs(hl.get_windows({ workspace = ws.id})) do
+        if w.title ~= "discord.com is sharing your screen." then
+            hl.dispatch(hl.dsp.focus({ window = w }))
+            return
+        end
+    end
+end)
