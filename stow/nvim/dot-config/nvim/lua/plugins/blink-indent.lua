c677e262072f3c6e6a9082dd13fee1f2c21630a6
Israel Ribeiro <israveri@gmail.com>
2026-06-19 05:36:20 -0300

    Customized blink.indent plugin

--- stow/nvim/dot-config/nvim/lua/plugins/blink-indent.lua
+++ stow/nvim/dot-config/nvim/lua/plugins/blink-indent.lua
@@ -1,3 +1,25 @@
 return {
     "saghen/blink.indent",
+    opts = {
+        static = {
+            enabled = true,
+            char = '▎',
+            whitespace_char = nil,
+            priority = 1,
+            highlights = { 'BlinkIndent' },
+        },
+
+        scope = {
+            enabled = true,
+            indent_at_cursor = true,
+            char = '▎',
+            priority = 1000,
+            highlights = { 'BlinkIndentViolet', 'BlinkIndentCyan', 'BlinkIndentBlue', 'BlinkIndentGreen', 'BlinkIndentYellow', 'BlinkIndentOrange', 'BlinkIndentRed' },
+
+            underline = {
+                enabled = true,
+                highlights = { 'BlinkIndentVioletUnderline', 'BlinkIndentCyanUnderline', 'BlinkIndentBlueUnderline', 'BlinkIndentGreenUnderline', 'BlinkIndentYellowUnderline', 'BlinkIndentOrangeUnderline', 'BlinkIndentRedUnderline' }
+            },
+        },
+    },
 }
