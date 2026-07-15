8e656afe61edf0994b23e19143625aee6f2727f4
Israel Ribeiro <israveri@gmail.com>
2026-07-15 02:11:30 -0300

    Integrates Neovim as external editor for Godot

---
+++ stow/nvim/dot-config/nvim/lua/plugins/godotdev.lua
@@ -0,0 +1,11 @@
+return {
+    "Mathijs-Bakker/godotdev.nvim",
+    dependencies = {
+        "nvim-dap",
+        "nvim-dap-ui",
+        "nvim-treesitter",
+    },
+    opts = {
+        csharp = true,
+    },
+}
