1931a4a92fa4d021cbe3f6a5024f3b8fa4c5cdbc
Israel Ribeiro <israveri@gmail.com>
2026-07-19 03:14:03 -0300

    Adds csakura installation and set it as wallpaper

--- stow/hypr/dot-config/hypr/core/autostart.lua
+++ stow/hypr/dot-config/hypr/core/autostart.lua
@@ -5,6 +5,9 @@ hl.on("hyprland.start", function ()
     -- Start clipboard history manager
     hl.exec_cmd("uwsm app -- wl-paste --watch cliphist store")
 
+    -- Csakura "wallpaper"
+    hl.exec_cmd("uwsm app -- kitten panel --edge 'center' --layer 'bottom' --output-name 'DP-2' csakura -f 10 -p 3 -c $(shuf -e sakura rose blush magenta peach coral sunset gold lavender violet sky mint matcha white ink -n 1)")
+
     -- Allows xdg-desktop-portal-hyprland to be called when needed
     -- Keep at the bottom
     hl.exec_cmd("uwsm app -- dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
